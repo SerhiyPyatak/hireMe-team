1e36b3b303ac75674c80ab5ff8796e2955c2588c
SerhiyPyatak <serhiypyatak@yahoo.com>
2021-03-08 15:37:56 +0200

    This commit adds a bunch of shallow fixes: hire-me form popup functionality, hover effects on buttons, and makes nav linksable to work

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function openForm() {
+    document.getElementById("hireForm").style.display = "block";
+    document.getElementById("curtain").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("hireForm").style.display = "none";
+    document.getElementById("curtain").style.display = "none";
+}
+
+function closeNav() {
+    document.getElementById("fixed-menu-check").click();
+}
